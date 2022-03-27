26e575fed79bd662a214dbcb22a3b54be44d5b4b
junjiexu628 <98210429+junjiexu628@users.noreply.github.com>
2022-03-27 20:08:16 +0800

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,133 @@
+# ---------------------------------------------------------------------
+# Project "Track 3D-Objects Over Time"
+# Copyright (C) 2020, Dr. Antje Muntzinger / Dr. Andreas Haja.
+#
+# Purpose of this file : Kalman filter class
+#
+# You should have received a copy of the Udacity license together with this program.
+#
+# https://www.udacity.com/course/self-driving-car-engineer-nanodegree--nd013
+# ----------------------------------------------------------------------
+#
+
+# imports
+import numpy as np
+
+# add project directory to python path to enable relative imports
+import os
+import sys
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+import misc.params as params 
+
+class Filter:
+    '''Kalman filter class'''
+    def __init__(self):
+        self.dt = params.dt
+        self.q = params.q
+        self.dim_state = params.dim_state
+        
+
+    def F(self):
+        ############
+        # TODO Step 1: implement and return system matrix F
+        ############
+        dt = self.dt
+
+        return np.matrix([[1,0,0,dt,0,0],
+                          [0,1,0,0,dt,0],
+                          [0,0,1,0,0,dt],
+                          [0,0,0,1,0,0],
+                          [0,0,0,0,1,0],
+                          [0,0,0,0,0,1]])
+        
+        ############
+        # END student code
+        ############ 
+
+    def Q(self):
+        ############
+        # TODO Step 1: implement and return process noise covariance Q
+        ############
+        dt = self.dt
+        q = self.q
+        q1 = q * dt**3 /3
+        q2 = q * dt**2 /2
+        q3 = q * dt
+        
+
+        return np.matrix([[q1,0,0,q2,0,0],
+                          [0,q1,0,0,q2,0],
+                          [0,0,q1,0,0,q2],
+                          [q2,0,0,q3,0,0],
+                          [0,q2,0,0,q3,0],
+                          [0,0,q2,0,0,q3]])
+        
+        ############
+        # END student code
+        ############ 
+
+    def predict(self, track):
+        ############
+        # TODO Step 1: predict state x and estimation error covariance P to next timestep, save x and P in track
+        ############
+        F = self.F()
+        x = F * track.x
+        P = F * track.P * F.transpose() + self.Q()
+        
+        track.x = x
+        track.P = P
+        
+        return track.x,track.P
+            
+        ############
+        # END student code
+        ############ 
+
+    def update(self, track, meas):
+        ############
+        # TODO Step 1: update state x and covariance P with associated measurement, save x and P in track
+        ############
+        dim_state = self.dim_state
+        H = meas.sensor.get_H(track.x)
+        gamma = self.gamma(track,meas)
+        S = self.S(track,meas,H)
+        K = track.P * H.transpose() * np.linalg.inv(S) 
+        x = track.x + K * gamma
+        I = np.identity((dim_state))
+        P = (I - K * H ) * track.P
+        
+        track.x = x
+        track.P = P
+        
+        ############
+        # END student code
+        ############ 
+        track.update_attributes(meas)
+        
+        return track.x,track.P
+    
+    def gamma(self, track, meas):
+        ############
+        # TODO Step 1: calculate and return residual gamma
+        ############
+        H = meas.sensor.get_H(track.x)
+        
+
+        return (meas.z - H * track.x)
+        
+        ############
+        # END student code
+        ############ 
+
+    def S(self, track, meas, H):
+        ############
+        # TODO Step 1: calculate and return covariance of residual S
+        ############
+
+        return (H * track.P * H.transpose() + meas.R)
+        
+        ############
+        # END student code
+        ############ 
